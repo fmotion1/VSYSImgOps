fcb5878cbbf28860fdc764eb27fd21289e57a2b4
fmotion1 <futuremotionrecovery@gmail.com>
2023-12-04 16:08:51 -0600

    renamed:    src/VSYSColorStructs/build_debug.ps1 -> src/Build/build_debug.ps1 renamed:    src/VSYSColorStructs/build_release.ps1 -> src/Build/build_release.ps1 renamed:    src/VSYSColorStructs/obj/Release/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs -> src/Dist/obj/Release/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.AssemblyInfo.cs -> src/Dist/obj/Release/net6.0/VSYSColorStructs.AssemblyInfo.cs renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.AssemblyInfoInputs.cache -> src/Dist/obj/Release/net6.0/VSYSColorStructs.AssemblyInfoInputs.cache renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.GeneratedMSBuildEditorConfig.editorconfig -> src/Dist/obj/Release/net6.0/VSYSColorStructs.GeneratedMSBuildEditorConfig.editorconfig renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.GlobalUsings.g.cs -> src/Dist/obj/Release/net6.0/VSYSColorStructs.GlobalUsings.g.cs renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.assets.cache -> src/Dist/obj/Release/net6.0/VSYSColorStructs.assets.cache renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.csproj.AssemblyReference.cache -> src/Dist/obj/Release/net6.0/VSYSColorStructs.csproj.AssemblyReference.cache renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.csproj.CoreCompileInputs.cache -> src/Dist/obj/Release/net6.0/VSYSColorStructs.csproj.CoreCompileInputs.cache renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.csproj.FileListAbsolute.txt -> src/Dist/obj/Release/net6.0/VSYSColorStructs.csproj.FileListAbsolute.txt renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.dll -> src/Dist/obj/Release/net6.0/VSYSColorStructs.dll renamed:    src/VSYSColorStructs/obj/Release/net6.0/VSYSColorStructs.sourcelink.json -> src/Dist/obj/Release/net6.0/VSYSColorStructs.sourcelink.json renamed:    src/VSYSColorStructs/obj/Release/net6.0/ref/VSYSColorStructs.dll -> src/Dist/obj/Release/net6.0/ref/VSYSColorStructs.dll renamed:    src/VSYSColorStructs/obj/Release/net6.0/refint/VSYSColorStructs.dll -> src/Dist/obj/Release/net6.0/refint/VSYSColorStructs.dll renamed:    src/VSYSColorStructs/obj/VSYSColorStructs.csproj.nuget.dgspec.json -> src/Dist/obj/VSYSColorStructs.csproj.nuget.dgspec.json renamed:    src/VSYSColorStructs/obj/VSYSColorStructs.csproj.nuget.g.props -> src/Dist/obj/VSYSColorStructs.csproj.nuget.g.props renamed:    src/VSYSColorStructs/obj/VSYSColorStructs.csproj.nuget.g.targets -> src/Dist/obj/VSYSColorStructs.csproj.nuget.g.targets renamed:    src/VSYSColorStructs/obj/project.assets.json -> src/Dist/obj/project.assets.json renamed:    src/VSYSColorStructs/obj/project.nuget.cache -> src/Dist/obj/project.nuget.cache new file:   src/VSYSColorOps/ColorConversion/HEXConverters.cs new file:   src/VSYSColorOps/ColorConversion/RGBConverters.cs new file:   src/VSYSColorOps/ColorStructs/ColorStructsFloat.cs renamed:    src/VSYSColorStructs/VSYSColorStructs.cs -> src/VSYSColorOps/ColorStructs/ColorStructsStandard.cs renamed:    src/VSYSColorStructs/VSYSColorStructs.csproj -> src/VSYSColorOps/VSYSColorOps.csproj

---
+++ src/VSYSColorOps/ColorConversion/RGBConverters.cs
@@ -0,0 +1,316 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VSYSColorOps.ColorConversion;
+
+namespace VSYSColorOps.ColorConversion
+{
+    public class RGBConverters
+    {
+        public static string RGBFloatToHex(float red, float green, float blue)
+        {
+            // Clamping the values between 0.0 and 1.0
+            red = Math.Clamp(red, 0.0f, 1.0f);
+            green = Math.Clamp(green, 0.0f, 1.0f);
+            blue = Math.Clamp(blue, 0.0f, 1.0f);
+
+            // Converting float values to integers in the 0-255 range
+            int r = (int)(red * 255);
+            int g = (int)(green * 255);
+            int b = (int)(blue * 255);
+
+            // Returning the formatted hex string
+            return $"#{r:X2}{g:X2}{b:X2}";
+        }
+
+        public static (float H, float S, float L) RGBFloatToHSL(float r, float g, float b)
+        {
+            // Clamp the RGB values to ensure they are between 0.0 and 1.0
+            r = Math.Clamp(r, 0.0f, 1.0f);
+            g = Math.Clamp(g, 0.0f, 1.0f);
+            b = Math.Clamp(b, 0.0f, 1.0f);
+
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+            float h, s, l;
+            l = (max + min) / 2;
+
+            if (max == min)
+            {
+                h = s = 0; // achromatic
+            }
+            else
+            {
+                float d = max - min;
+                s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+                if (max == r)
+                {
+                    h = (g - b) / d + (g < b ? 6 : 0);
+                }
+                else if (max == g)
+                {
+                    h = (b - r) / d + 2;
+                }
+                else // max == b
+                {
+                    h = (r - g) / d + 4;
+                }
+
+                h /= 6;
+            }
+
+            // Convert to degrees, percentage
+            h = h * 360; // Hue in degrees
+            s = s * 100; // Saturation in percentage
+            l = l * 100; // Lightness in percentage
+
+            return (h, s, l);
+        }
+
+        public static (float H, float S, float L) RGBFloatToHSLFloat(float r, float g, float b)
+        {
+            // Clamp the RGB values to ensure they are between 0.0 and 1.0
+            r = Math.Clamp(r, 0.0f, 1.0f);
+            g = Math.Clamp(g, 0.0f, 1.0f);
+            b = Math.Clamp(b, 0.0f, 1.0f);
+
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+            float h, s, l;
+            l = (max + min) / 2;
+
+            if (max == min)
+            {
+                h = s = 0; // achromatic
+            }
+            else
+            {
+                float d = max - min;
+                s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+                if (max == r)
+                {
+                    h = (g - b) / d + (g < b ? 6 : 0);
+                }
+                else if (max == g)
+                {
+                    h = (b - r) / d + 2;
+                }
+                else
+                {
+                    h = (r - g) / d + 4;
+                }
+
+                h /= 6;
+            }
+
+            return (h, s, l);
+        }
+
+        public static (float H, float S, float V) RGBFloatToHSVFloat(float r, float g, float b)
+        {
+            // Clamp the RGB values to ensure they are between 0.0 and 1.0
+            r = Math.Clamp(r, 0.0f, 1.0f);
+            g = Math.Clamp(g, 0.0f, 1.0f);
+            b = Math.Clamp(b, 0.0f, 1.0f);
+
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+            float h, s, v;
+            v = max;
+
+            float delta = max - min;
+            s = max == 0 ? 0 : delta / max;
+
+            if (max == min)
+            {
+                h = 0; // achromatic
+            }
+            else
+            {
+                if (max == r)
+                {
+                    h = (g - b) / delta + (g < b ? 6 : 0);
+                }
+                else if (max == g)
+                {
+                    h = (b - r) / delta + 2;
+                }
+                else
+                {
+                    h = (r - g) / delta + 4;
+                }
+
+                h /= 6;
+            }
+
+            return (h, s, v);
+        }
+
+        public static (float H, float S, float V) RGBFloatToHSV(float r, float g, float b)
+        {
+            // Clamp the RGB values to ensure they are between 0.0 and 1.0
+            r = Math.Clamp(r, 0.0f, 1.0f);
+            g = Math.Clamp(g, 0.0f, 1.0f);
+            b = Math.Clamp(b, 0.0f, 1.0f);
+
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+            float h, s, v;
+            v = max;
+
+            float delta = max - min;
+            s = max == 0 ? 0 : delta / max;
+
+            if (max == min)
+            {
+                h = 0; // achromatic
+            }
+            else
+            {
+                if (max == r)
+                {
+                    h = (g - b) / delta + (g < b ? 6 : 0);
+                }
+                else if (max == g)
+                {
+                    h = (b - r) / delta + 2;
+                }
+                else
+                {
+                    h = (r - g) / delta + 4;
+                }
+
+                h /= 6;
+            }
+
+            // Convert to degrees and percentage
+            h = h * 360; // Hue in degrees
+            s = s * 100; // Saturation in percentage
+            v = v * 100; // Value in percentage
+
+            return (h, s, v);
+        }
+
+        public static (float C, float M, float Y, float K) RGBFloatToCMYKFloat(float r, float g, float b)
+        {
+            float k = 1 - Math.Max(Math.Max(r, g), b);
+
+            if (k == 1)
+            {
+                return (0, 0, 0, 1); // Pure black
+            }
+
+            float c = (1 - r - k) / (1 - k);
+            float m = (1 - g - k) / (1 - k);
+            float y = (1 - b - k) / (1 - k);
+
+            return (c, m, y, k);
+        }
+
+        public static (float C, float M, float Y, float K) RGBFloatToCMYKPercentage(float r, float g, float b)
+        {
+            // Ensure the RGB values are in the 0.0 to 1.0 range
+            r = Math.Clamp(r, 0.0f, 1.0f);
+            g = Math.Clamp(g, 0.0f, 1.0f);
+            b = Math.Clamp(b, 0.0f, 1.0f);
+
+            // Calculate Key (Black) value
+            float k = 1 - Math.Max(Math.Max(r, g), b);
+
+            if (k == 1)
+            {
+                return (0, 0, 0, 100); // Pure black
+            }
+
+            // Calculate Cyan, Magenta, Yellow values
+            float c = (1 - r - k) / (1 - k) * 100;
+            float m = (1 - g - k) / (1 - k) * 100;
+            float y = (1 - b - k) / (1 - k) * 100;
+            float kPercentage = k * 100;
+
+            return (c, m, y, kPercentage);
+        }
+
+        public static string RGBByteToHex(byte red, byte green, byte blue)
+        {
+            // Returning the formatted hex string
+            return $"#{red:X2}{green:X2}{blue:X2}";
+        }
+
+        public static (float H, float S, float V) RGBByteToHSVFloat(byte r, byte g, byte b)
+        {
+            float rf = r / 255f;
+            float gf = g / 255f;
+            float bf = b / 255f;
+
+            float max = Math.Max(rf, Math.Max(gf, bf));
+            float min = Math.Min(rf, Math.Min(gf, bf));
+            float h, s, v;
+            v = max;
+
+            float delta = max - min;
+            s = max == 0 ? 0 : delta / max;
+
+            if (max == min)
+            {
+                h = 0; // Achromatic
+            }
+            else
+            {
+                if (max == rf)
+                {
+                    h = (gf - bf) / delta + (gf < bf ? 6 : 0);
+                }
+                else if (max == gf)
+                {
+                    h = (bf - rf) / delta + 2;
+                }
+                else
+                {
+                    h = (rf - gf) / delta + 4;
+                }
+
+                h /= 6;
+            }
+
+            return (h, s, v);
+        }
+
+        public static (float C, float M, float Y, float K) RGBByteToCMYKFloat(byte r, byte g, byte b)
+        {
+            float rf = r / 255f;
+            float gf = g / 255f;
+            float bf = b / 255f;
+
+            return RGBFloatToCMYK(rf, gf, bf);
+        }
+        
+        public static (float C, float M, float Y, float K) RGBByteToCMYKPercentage(byte r, byte g, byte b)
+        {
+            // Normalize the RGB values to the range 0.0 to 1.0
+            float rf = r / 255f;
+            float gf = g / 255f;
+            float bf = b / 255f;
+
+            // Calculate Key (Black) value
+            float k = 1 - Math.Max(Math.Max(rf, gf), bf);
+
+            if (k == 1)
+            {
+                return (0, 0, 0, 100); // Pure black
+            }
+
+            // Calculate Cyan, Magenta, Yellow values
+            float c = (1 - rf - k) / (1 - k) * 100;
+            float m = (1 - gf - k) / (1 - k) * 100;
+            float y = (1 - bf - k) / (1 - k) * 100;
+            float kPercentage = k * 100;
+
+            return (c, m, y, kPercentage);
+        }
+
+    }
+}
---
+++ src/VSYSColorOps/ColorStructs/ColorStructsFloat.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// The VSYSColorOps.ColorStructs namespace contains color-related struct definitions used for various color operations in the Visual System (VSYS) project.
+/// </summary>
+namespace VSYSColorOps.ColorStructs
+{
+
+    /// <summary>
+    /// Represents a color using red, green, and blue channels where the values range from 0 to 1.
+    /// </summary>
+    public sealed class RGBFloat
+    {
+        /// <summary>
+        /// Gets the value of the red channel ranging from 0 to 1.
+        /// </summary>
+        public double Red { get; }
+    
+        /// <summary>
+        /// Gets the value of the green channel ranging from 0 to 1.
+        /// </summary>
+        public double Green { get; }
+    
+        /// <summary>
+        /// Gets the value of the blue channel ranging from 0 to 1.
+        /// </summary>
+        public double Blue { get; }
+    
+        /// <summary>
+        /// Initializes a new instance of the RGBFloat class with specified red, green, and blue channel values.
+        /// </summary>
+        /// <param name="red">The red channel value (0-1).</param>
+        /// <param name="green">The green channel value (0-1).</param>
+        /// <param name="blue">The blue channel value (0-1).</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when any of the input parameters are less than 0 or greater than 1.
+        /// </exception>
+        public RGBFloat(double red, double green, double blue)
+        {
+            if(red > 1 || red < 0)
+                throw new ArgumentOutOfRangeException(nameof(red),
+                "Red channel is out of bounds (0-1).");
+    
+            if(green > 1 || green < 0)
+                throw new ArgumentOutOfRangeException(nameof(green),
+                "Green channel is out of bounds (0-1).");
+    
+            if(blue > 1 || blue < 0)
+                throw new ArgumentOutOfRangeException(nameof(blue),
+                "Blue channel is out of bounds (0-1).");
+    
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string in the format "(red,green,blue)".</returns>
+        public override string ToString()
+        {
+            return $"({Red},{Green},{Blue})";
+        }
+    }
+
+
+    /// <summary>
+    /// Defines a RGBA color using floating point values between 0 and 1.
+    /// </summary>
+    public sealed class RGBAFloat
+    {
+        /// <summary>
+        /// Gets the Red component of the color.
+        /// </summary>
+        public double Red { get; }
+    
+        /// <summary>
+        /// Gets the Green component of the color.
+        /// </summary>
+        public double Green { get; }
+    
+        /// <summary>
+        /// Gets the Blue component of the color.
+        /// </summary>
+        public double Blue { get; }
+    
+        /// <summary>
+        /// Gets the Alpha transparency level of the color.
+        /// </summary>
+        public double Alpha { get; }
+    
+    
+        /// <summary>
+        /// Initializes a new instance of the RGBAFloat class.
+        /// </summary>
+        /// <param name="red">The red component of the color. Must be between 0 and 1.</param>
+        /// <param name="green">The green component of the color. Must be between 0 and 1.</param>
+        /// <param name="blue">The blue component of the color. Must be between 0 and 1.</param>
+        /// <param name="alpha">The alpha transparency level of the color. Must be between 0 and 1.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when any of the parameters are less than 0 or greater than 1.
+        /// </exception>
+        public RGBAFloat(double red, double green, double blue, double alpha)
+        {
+            if(red > 1 || red < 0)
+                throw new ArgumentOutOfRangeException(nameof(red),
+                "Red channel is out of bounds (0-1).");
+
+            if(green > 1 || green < 0)
+                throw new ArgumentOutOfRangeException(nameof(green),
+                "Green channel is out of bounds (0-1).");
+
+            if(blue > 1 || blue < 0)
+                throw new ArgumentOutOfRangeException(nameof(blue),
+                "Blue channel is out of bounds (0-1).");
+
+            if(alpha > 1 || alpha < 0)
+                throw new ArgumentOutOfRangeException(nameof(alpha),
+                "Alpha channel is out of bounds (0-1).");
+
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+    
+        /// <summary>
+        /// Returns a string representation of the RGBAFloat in the format (red,green,blue,alpha).
+        /// </summary>
+        /// <returns>A string representing the color.</returns>
+        public override string ToString()
+        {
+            return $"({Red},{Green},{Blue},{Alpha})";
+        }
+    }
+}
