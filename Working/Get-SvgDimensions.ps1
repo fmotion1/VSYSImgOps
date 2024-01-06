abcb62fd000396c63cf89e8d220400bff102b13e
fmotion1 <futuremotionrecovery@gmail.com>
2024-01-06 17:54:54 -0600

    Remove unused functions and comments

---
+++ Working/Get-SvgDimensions.ps1
@@ -0,0 +1,22 @@
+﻿
+
+function Get-SvgDimensions {
+    param([string]$path)
+    $content = Get-Content -Path $path -Raw
+
+    if ($content -match 'viewBox="([^"]+)"') {
+        $viewBoxValues = $matches[1].Split(' ')
+        $width = $viewBoxValues[2]
+        $height = $viewBoxValues[3]
+    } elseif ($content -match 'width="([^"]+)"' -and $content -match 'height="([^"]+)"') {
+        $width = $matches[1]
+        $height = $matches[1]
+    } else {
+        $width = "Unknown"
+        $height = "Unknown"
+    }
+
+    return "Width: $width, Height: $height"
+}
+
+Get-SvgDimensions "C:\Users\futur\Desktop\Test\Uncropped\Brands Coinbase.svg"
